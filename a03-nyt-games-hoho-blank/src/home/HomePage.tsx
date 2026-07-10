d6d8c364e232704c53f2189dba9505e02fe11202
hoho <lche417@aucklanduni.ac.nz>
2026-07-11 11:29:32 +1200

    Insert the author name

--- a03-nyt-games-hoho-blank/src/home/HomePage.tsx
+++ a03-nyt-games-hoho-blank/src/home/HomePage.tsx
@@ -6,10 +6,7 @@ import Header from "../ui/Header";
 import GameCard from "./GameCard";
 
 export default function HomePage() {
-  /**
-   * TODO:
-   * Change the name of `Partner 1` and `Partner 2` to your names!
-   */
+  
   return (
     <div className="flex h-svh max-h-svh w-full flex-col items-center overflow-hidden">
       <Header />
@@ -20,11 +17,7 @@ export default function HomePage() {
       <p className="h-14 px-4 text-center text-xs text-gray-600">
         Made with 🩵 by{" "}
         <a className="underline underline-offset-2 hover:cursor-pointer">
-          Partner 1
-        </a>{" "}
-        and{" "}
-        <a className="underline underline-offset-2 hover:cursor-pointer">
-          Partner 2
+          Hoho
         </a>{" "}
         for COMP 426 @ UNC-Chapel Hill
       </p>

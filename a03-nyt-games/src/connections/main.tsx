9e215be50ef603a8ce8911e3cf62ab3910fd33d9
hoho <lche417@aucklanduni.ac.nz>
2026-07-15 09:34:23 +1200

    Delete a03-nyt-games directory

--- a03-nyt-games/src/connections/main.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * DO NOT MODIFY
- * Main entrypoint for the Connections game React page.
- */
-import { createRoot } from "react-dom/client";
-import ConnectionsPage from "./ConnectionsPage";
-
-const root = createRoot(document.getElementById("root")!);
-root.render(<ConnectionsPage />);
